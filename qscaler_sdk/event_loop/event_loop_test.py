2ff84f742a0f41d0e82cc04f8f031a04ebc0ebeb
Omri Assa <omriassa@gmail.com>
2025-01-04 18:00:51 +0200

    test: handling of test fail

--- qscaler_sdk/event_loop/event_loop_test.py
+++ qscaler_sdk/event_loop/event_loop_test.py
@@ -10,8 +10,8 @@ from qscaler_sdk.event_loop.event_loop import EventLoop, ForceShutdown
 
 class TestEventLoop:
 
-    @pytest.mark.parametrize("actuall_signal", [signal.SIGTERM, signal.SIGINT])
-    def test_force_shutdown_on_sigterm_sigint_signal_should_run_force_exit(self, mocker, actuall_signal):
+    @pytest.mark.parametrize("actual_signal", [signal.SIGTERM, signal.SIGINT])
+    def test_force_shutdown_on_sigterm_sigint_signal_should_run_force_exit(self, mocker, actual_signal):
         # Arrange
         mock_force_shutdown = mocker.Mock()
         event_loop = EventLoop(check_for_death= lambda: None,
@@ -22,15 +22,18 @@ class TestEventLoop:
         def send_sigterm_later():
             # wait 2 seconds then send signal
             time.sleep(2)
-            signal.raise_signal(actuall_signal)
-
+            signal.raise_signal(actual_signal)
 
         # Start the thread that will send SIGTERM after 10 seconds
         thread = threading.Thread(target=send_sigterm_later)
         thread.start()
 
-        # Act
-        event_loop()
+
+        try:
+            # Act
+            event_loop()
+        except KeyboardInterrupt:
+            pytest.fail("sigint siganl was not handled in the event_loop()")
 
         mock_force_shutdown.assert_called_once()
         assert not thread.is_alive()
